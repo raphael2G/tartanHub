8fcdadfa57d2f787eb5e6273d0a4c02f4de78798
bigFraug <tqr@andrew.cmu.edu>
2024-05-29 11:19:56 -0400

    integer while running, decimal while stopped

--- screens/stopperScreen/components/stopwatchDisplay.tsx
+++ screens/stopperScreen/components/stopwatchDisplay.tsx
@@ -6,11 +6,11 @@ interface StopwatchDisplayProps {
   isServe: boolean;
 }
 
-const formatTime = (time: number, isServe: boolean) => {
+const formatTime = (time: number, isRunning: boolean) => {
   const milliseconds = time % 1000;
   const seconds = Math.floor(time / 1000);
 
-  return isServe ? `${seconds.toString()}` : `${seconds.toString()}.${milliseconds.toString().padStart(3, '0')}`;
+  return isRunning ? `${seconds.toString()}` : `${seconds.toString()}.${milliseconds.toString().padStart(3, '0')}`;
 };
 
 const StopwatchDisplay: React.FC<StopwatchDisplayProps> = ({ time, isServe }) => {
