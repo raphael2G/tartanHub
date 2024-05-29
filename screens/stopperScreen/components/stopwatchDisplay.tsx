aef7f7e39a67dbf1b452245a43723eb972de8fce
bigFraug <tqr@andrew.cmu.edu>
2024-05-29 12:25:11 -0400

    trying again to fix stopwatch formatting

--- screens/stopperScreen/components/stopwatchDisplay.tsx
+++ screens/stopperScreen/components/stopwatchDisplay.tsx
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 interface StopwatchDisplayProps {
   time: number;
   isServe: boolean;
+  isRunning: boolean;
 }
 
 const formatTime = (time: number, isRunning: boolean) => {
@@ -13,10 +14,10 @@ const formatTime = (time: number, isRunning: boolean) => {
   return isRunning ? `${seconds.toString()}` : `${seconds.toString()}.${milliseconds.toString().padStart(3, '0')}`;
 };
 
-const StopwatchDisplay: React.FC<StopwatchDisplayProps> = ({ time, isServe }) => {
+const StopwatchDisplay: React.FC<StopwatchDisplayProps> = ({ time, isServe, isRunning }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.time}>{formatTime(time, isServe)}</Text>
+      <Text style={styles.time}>{formatTime(time, isRunning)}</Text>
     </View>
   );
 };
